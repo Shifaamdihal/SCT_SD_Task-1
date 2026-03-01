deb09d9b84dd5fced68a8067b8077fd3b1603554
Shifaamdihal <shifaamdihal@gmail.com>
2026-03-01 22:05:43 +0530

    Add files via upload
    Added Task-1

---
+++ TemperatureConverter.java
@@ -0,0 +1,23 @@
+package Pack;
+
+	import java.util.Scanner;
+
+	public class TemperatureConverter {
+	    public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter temperature in Celsius: ");
+	        double c = sc.nextDouble();
+
+	        double f = (c * 9/5) + 32;
+	        double k = c + 273.15;
+
+	        System.out.println("Fahrenheit: " + f);
+	        System.out.println("Kelvin: " + k);
+
+	        sc.close();
+	    }
+	}
+
+
